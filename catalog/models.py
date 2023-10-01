a27441904d86e758f647c08e945ca20bf1124569
guzel <guzel.kh23@gmail.ru>
2023-10-01 16:36:07 +0300

    4 is done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    creation_date = models.DateTimeField(verbose_name='дата создания')
+    mod_date = models.DateTimeField(verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.preview} {self.category} ' \
+               f'{self.price} {self.creation_date} {self.mod_date}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
