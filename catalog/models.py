f129af26291ccd358a1b619ae9a4c519773cdd26
guzel <guzel.kh23@gmail.ru>
2023-10-04 21:45:31 +0300

    6 is done

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
 
     def __str__(self):
         return f'{self.name}'
@@ -16,16 +16,15 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
     preview = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена за покупку')
-    creation_date = models.DateTimeField(verbose_name='дата создания')
-    mod_date = models.DateTimeField(verbose_name='дата изменения')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    mod_date = models.DateTimeField(auto_now=True, verbose_name='дата изменения')
 
     def __str__(self):
-        return f'{self.name} {self.description} {self.preview} {self.category} ' \
-               f'{self.price} {self.creation_date} {self.mod_date}'
+        return f'{self.name} {self.category} {self.price} '
 
     class Meta:
         verbose_name = 'продукт'
