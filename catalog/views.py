5c8b36e8953ad0be7f684945e1a603c19e93b50d
guzel <guzel.kh23@gmail.ru>
2023-10-24 21:48:24 +0300

    Directory static mooved

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Продукты'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
@@ -12,4 +18,13 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(id=pk),
+        'title': f'Продукт {product_item.name}'
+    }
+    return render(request, 'catalog/product.html', context)
 
