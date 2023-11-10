0851345bdccc534bab76072949367ccc8cd90b5f
guzel <guzel.kh23@gmail.ru>
2023-11-10 21:51:29 +0300

    HW part 1: FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product
+from catalog.views import contacts, ProductsListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contacts),
-    path('<int:pk>/catalog/', product, name='product')
+    path('', ProductsListView.as_view(), name='index'),
+    path('contacts/', contacts, name='contact'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='view_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def index(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title': 'Продукты'
-    }
-    return render(request, 'catalog/index.html', context)
+# def index(request):
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title': 'Продукты'
+#     }
+#     return render(request, 'catalog/index.html', context)
+
+class ProductsListView(ListView):
+    model = Product
+    extra_context = {'title': 'Продукты'}
 
 
 def contacts(request):
@@ -20,11 +25,16 @@ def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def product(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(id=pk),
-        'title': f'Продукт {product_item.name}'
-    }
-    return render(request, 'catalog/product.html', context)
+# def product(request, pk):
+#     product_item = Product.objects.get(pk=pk)
+#     context = {
+#         'object_list': Product.objects.filter(id=pk),
+#         'title': f'Продукт {product_item.name}'
+#     }
+#     return render(request, 'catalog/product.html', context)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html/'
 
